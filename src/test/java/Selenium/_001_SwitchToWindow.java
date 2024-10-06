2feba41e5fa659751ce979027a84c9362538dbb1
avdhutssh <avdhut.ssh@gmail.com>
2024-10-07 00:21:13 +0530

    Switch to window Using Stream and Array

--- src/test/java/Selenium/_001_SwitchToWindow.java
+++ src/test/java/Selenium/_001_SwitchToWindow.java
@@ -19,6 +19,7 @@ public class _001_SwitchToWindow {
 	WebDriverWait wt;
 	WebDriver driver;
 	JavascriptExecutor jse;
+	int windowNumber = 2;
 
 	@BeforeTest
 	private void setup() {
@@ -53,6 +54,38 @@ public class _001_SwitchToWindow {
 		}
 		driver.close();
 		driver.switchTo().window(parentWindowID);
+		Assert.assertEquals("DEMOQA", driver.getTitle());
+	}
+
+	@Test
+	private void _01_Using_Stream_Array() {
+		driver.manage().window().maximize();
+		driver.get("https://demoqa.com/browser-windows");
+		WebElement windowBtn = driver.findElement(By.id("windowButton"));
+		jse.executeScript("arguments[0].scrollIntoView(true)", windowBtn);
+		windowBtn.click();
+
+		String[] windowHandles = driver.getWindowHandles().stream().toArray(String[]::new);
+		System.out.println("windowHandles.length: " + windowHandles.length);
+		try {
+			if (windowNumber > windowHandles.length) {
+				throw new RuntimeException(String.format(
+						"The specified window number (%s) is greater than the " + "number of windows created (%s).",
+						windowNumber, windowHandles.length));
+			}
+			driver.switchTo().window(windowHandles[windowNumber - 1]);
+			System.out.println("Successfully switched to window number: " + windowNumber);
+		} catch (Exception ex) {
+			throw new RuntimeException(
+					String.format("Failed to switch to window number %s. The total number of open windows was %s.",
+							windowNumber, windowHandles.length),
+					ex);
+
+		}
+		String pageHeading = driver.findElement(By.id("sampleHeading")).getText();
+		System.out.println("Heading of child window is " + pageHeading);
+		driver.close();
+		driver.switchTo().window(windowHandles[0]);
 		Assert.assertEquals("DEMOQA", driver.getTitle());
 	}
 }
