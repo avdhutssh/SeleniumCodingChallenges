23cd48f8a8dbe7c1519e19001c5914f083fc1183
avdhutssh <avdhut.ssh@gmail.com>
2024-10-09 11:15:43 +0530

    _03_compareArrayAndFindCommon

--- src/test/java/DoubtsVerification/_01_PracticeJava.java
+++ src/test/java/DoubtsVerification/_01_PracticeJava.java
@@ -1,12 +1,26 @@
 package DoubtsVerification;
 
+import java.util.ArrayList;
+
 public class _01_PracticeJava {
 
 	public static void main(String[] args) {
 //		System.out.println(_01_multiply(5, 10)); // 50
-		int[] arr1 = { 1, 4, 8, 15, 17 };
-		System.out.println(_02_MaxDiffBetweenAdjacentElementsInArr(arr1)); // 7
+		
+//		int[] arr1 = { 1, 4, 8, 15, 17 };
+//		System.out.println(_02_MaxDiffBetweenAdjacentElementsInArr(arr1)); // 7
+		
+		int[] a = { 1, 4, 5, 7 };
+		int[] b = { 6, 4, 3, 7 };
+		_03_compareArrayAndFindCommon(a, b);
 
+	}
+
+	public static void _03_compareArrayAndFindCommon(int[] a, int[] b) {
+
+		for(int i=0;i<a.length;i++) {
+			if(a[i]==b[i]) System.out.print(a[i]+" ");
+		}
 	}
 
 	private static int _02_MaxDiffBetweenAdjacentElementsInArr(int[] arr) {
