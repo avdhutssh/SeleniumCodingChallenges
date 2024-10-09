d992bc058bf652c1554adb65aac0b83550995d3d
avdhutssh <avdhut.ssh@gmail.com>
2024-10-09 11:19:18 +0530

    _04_Scanner

--- src/test/java/DoubtsVerification/_01_PracticeJava.java
+++ src/test/java/DoubtsVerification/_01_PracticeJava.java
@@ -1,25 +1,47 @@
 package DoubtsVerification;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class _01_PracticeJava {
 
 	public static void main(String[] args) {
 //		System.out.println(_01_multiply(5, 10)); // 50
-		
+
 //		int[] arr1 = { 1, 4, 8, 15, 17 };
 //		System.out.println(_02_MaxDiffBetweenAdjacentElementsInArr(arr1)); // 7
-		
-		int[] a = { 1, 4, 5, 7 };
-		int[] b = { 6, 4, 3, 7 };
-		_03_compareArrayAndFindCommon(a, b);
 
+//		int[] a = { 1, 4, 5, 7 };
+//		int[] b = { 6, 4, 3, 7 };
+//		_03_compareArrayAndFindCommon(a, b);
+
+		_04_Scanner();
+	}
+
+	private static void _04_Scanner() {
+		int a;
+		float b;
+		String c;
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter integer");
+		a = sc.nextInt();
+		System.out.println("integer entered is: " + a);
+
+		System.out.println("Enter float");
+		b = sc.nextFloat();
+		System.out.println("float entered is: " + b);
+
+		System.out.println("Enter string");
+		c = sc.next();
+		System.out.println("string entered is: " + c);
 	}
 
 	public static void _03_compareArrayAndFindCommon(int[] a, int[] b) {
 
-		for(int i=0;i<a.length;i++) {
-			if(a[i]==b[i]) System.out.print(a[i]+" ");
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] == b[i])
+				System.out.print(a[i] + " ");
 		}
 	}
 
