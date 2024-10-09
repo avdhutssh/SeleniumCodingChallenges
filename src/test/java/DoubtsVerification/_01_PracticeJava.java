70e6e4ed0b070971cce2654e1c3b548ec9acfd52
avdhutssh <avdhut.ssh@gmail.com>
2024-10-09 10:54:54 +0530

    _02_MaxDiffBetweenAdjacentElementsInArr

--- src/test/java/DoubtsVerification/_01_PracticeJava.java
+++ src/test/java/DoubtsVerification/_01_PracticeJava.java
@@ -3,15 +3,31 @@ package DoubtsVerification;
 public class _01_PracticeJava {
 
 	public static void main(String[] args) {
-		System.out.println(_01_multiply(5, 10));
+//		System.out.println(_01_multiply(5, 10)); // 50
+		int[] arr1 = { 1, 4, 8, 15, 17 };
+		System.out.println(_02_MaxDiffBetweenAdjacentElementsInArr(arr1)); // 7
 
 	}
 
+	private static int _02_MaxDiffBetweenAdjacentElementsInArr(int[] arr) {
+		// Time complexity: O(n)
+		// Space complexity: O(1)
+		int maxDiff = Integer.MIN_VALUE;
+		for (int i = 0; i < arr.length - 1; i++) {
+			int diff = arr[i + 1] - arr[i];
+			maxDiff = Math.max(maxDiff, diff);
+		}
+
+		return maxDiff;
+	}
+
 	private static int _01_multiply(int i, int j) {
-		int sum =0;
-		int k =1;
-		while(k<=j) {
-			sum+=i;
+		// Time complexity: O(j)
+		// Space complexity: O(1)
+		int sum = 0;
+		int k = 1;
+		while (k <= j) {
+			sum += i;
 			k++;
 		}
 
