17a2c40fc96de1c8a258568931b1f8f1d17d7783
avdhutssh <avdhut.ssh@gmail.com>
2024-10-15 18:12:19 +0530

    _51_longestConsecutiveOccurrenceOfIntegersInArray_BFA

--- src/test/java/DoubtsVerification/_01_PracticeJava.java
+++ src/test/java/DoubtsVerification/_01_PracticeJava.java
@@ -218,17 +218,37 @@ public class _01_PracticeJava {
 //		char[] arr = { 'a', 'b', 'c', 'd', 'e' }; // e d c b a
 //		System.out.println(Arrays.toString(_47_reverseArray_Using_Stack(arr)));
 
-		String str1 = "AvdhutSatishShirgaonkar"; // 3
-		String str2 = "UnderTaker Dead$%Man"; // 4
+//		String str1 = "AvdhutSatishShirgaonkar"; // 3
+//		String str2 = "UnderTaker Dead$%Man"; // 4
+//
+//		System.out.println("Using BFA: " + _48_countCapitalizedWordsInString_BFA(str1));
+//		System.out.println("Using BFA: " + _48_countCapitalizedWordsInString_BFA(str2));
+//
+//		System.out.println("Using RegX: " + _49_countCapitalizedWordsInString_RegX(str1));
+//		System.out.println("Using RegX: " + _49_countCapitalizedWordsInString_RegX(str2));
+//		
+//		System.out.println("Using Stream: " + _50_countCapitalizedWordsInString_Stream(str1));
+//		System.out.println("Using Stream: " + _50_countCapitalizedWordsInString_Stream(str2));
 
-		System.out.println("Using BFA: " + _48_countCapitalizedWordsInString_BFA(str1));
-		System.out.println("Using BFA: " + _48_countCapitalizedWordsInString_BFA(str2));
+		int[] arr = { 4, 3, 25, 6, 7, 8, 9, 2, 3, 10 }; // 4
+		System.out.println(_51_longestConsecutiveOccurrenceOfIntegersInArray_BFA(arr));
 
-		System.out.println("Using RegX: " + _49_countCapitalizedWordsInString_RegX(str1));
-		System.out.println("Using RegX: " + _49_countCapitalizedWordsInString_RegX(str2));
-		
-		System.out.println("Using Stream: " + _50_countCapitalizedWordsInString_Stream(str1));
-		System.out.println("Using Stream: " + _50_countCapitalizedWordsInString_Stream(str2));
+	}
+
+	private static int _51_longestConsecutiveOccurrenceOfIntegersInArray_BFA(int[] arr) {
+
+		int count = 0;
+		int max = 0;
+		for (int i = 0; i < arr.length - 1; i++) {
+			if (arr[i] + 1 == arr[i + 1])
+				count++;
+			else
+				count = 0;
+
+			max = Math.max(max, count+1);
+		}
+
+		return max;
 	}
 
 	private static int _50_countCapitalizedWordsInString_Stream(String str) {
