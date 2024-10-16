0e22173c784970082ed0ca204fda8fb530a78dcc
avdhutssh <avdhut.ssh@gmail.com>
2024-10-16 13:47:06 +0530

    _62_shiftAllZerosOnRight_Using_InPlaceApproach

--- src/test/java/DoubtsVerification/_01_PracticeJava.java
+++ src/test/java/DoubtsVerification/_01_PracticeJava.java
@@ -260,7 +260,27 @@ public class _01_PracticeJava {
 //		_60_printNumbers1to100_WithoutUsingAnyLoop(1);
 
 		int[] arr1 = { 1, 0, 2, 0, 3, 0, 0, 0 }; // 1, 2, 3, 0, 0, 0, 0, 0
-		System.out.println("Using Brute Force Approach: " + Arrays.toString(_61_shiftAllZerosOnRight_BFA(arr1)));
+//		System.out.println("Using Brute Force Approach: " + Arrays.toString(_61_shiftAllZerosOnRight_BFA(arr1)));
+
+		System.out.println(
+				"Using In Place Approach: " + Arrays.toString(_62_shiftAllZerosOnRight_Using_InPlaceApproach(arr1)));
+
+	}
+
+	private static int[] _62_shiftAllZerosOnRight_Using_InPlaceApproach(int[] arr) {
+		// Time Complexity: O(n)
+		// Space Complexity: O(1)
+
+		int index = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] != 0) {
+				arr[index++] = arr[i];
+			}
+		}
+		while (index < arr.length) {
+			arr[index++] = 0;
+		}
+		return arr;
 	}
 
 	private static int[] _61_shiftAllZerosOnRight_BFA(int[] arr) {
