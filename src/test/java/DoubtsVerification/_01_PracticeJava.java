6a39778bfcdfe2f5ac4d48866ba150cbe55d3768
avdhutssh <avdhut.ssh@gmail.com>
2024-10-16 11:47:00 +0530

    _55_productOfAnArrayExceptSelf_PrefixSuffix

--- src/test/java/DoubtsVerification/_01_PracticeJava.java
+++ src/test/java/DoubtsVerification/_01_PracticeJava.java
@@ -242,7 +242,34 @@ public class _01_PracticeJava {
 		int[] arr = { 10, 3, 5, 6, 2 }; // Output: prod[] = {180, 600, 360, 300, 900}
 
 		System.out.println("Using BFA : " + Arrays.toString(_54_productOfAnArrayExceptSelf_BFA(arr)));
+		System.out.println("Using Prefix and Suffix : " + Arrays.toString(_55_productOfAnArrayExceptSelf_PrefixSuffix(arr)));
 
+	}
+
+	private static int[] _55_productOfAnArrayExceptSelf_PrefixSuffix(int[] arr) {
+		// Time Complexity: O(n)
+		// Space Complexity: O(n)
+		int len = arr.length;
+		int[] left = new int[arr.length];
+		int[] right = new int[arr.length];
+		int[] prod = new int[arr.length];
+		
+		left[0]= 1;
+		right[len-1]=1;
+		
+		for(int i=1;i<left.length;i++) {
+			left[i] = arr[i-1] * left[i-1];
+		}
+		
+		for(int i=len-2;i>=0;i--) {
+			right[i] = arr[i+1] * right[i+1];
+		}
+		
+		for(int i=0;i<prod.length;i++) {
+			prod[i] = left[i] * right[i];
+		}
+		
+		return prod;
 	}
 
 	private static int[] _54_productOfAnArrayExceptSelf_BFA(int[] arr) {
