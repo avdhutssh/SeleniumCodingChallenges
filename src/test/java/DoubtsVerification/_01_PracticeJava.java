de16da9622572f47a655c23cb92b567deeb6e435
avdhutssh <avdhut.ssh@gmail.com>
2024-10-16 12:44:09 +0530

    _59_first_LastPositionOfNumberInarray_Using_LinearIteration

--- src/test/java/DoubtsVerification/_01_PracticeJava.java
+++ src/test/java/DoubtsVerification/_01_PracticeJava.java
@@ -248,8 +248,31 @@ public class _01_PracticeJava {
 //		System.out.println("Using BFA : " + _56_missingNumberInGivenArray_BFA(arr));
 //		System.out.println("Using HashSet : " + _57_missingNumberInGivenArray_HashSet(arr));
 
-		int[] arr = { 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1 }; // 5
-		System.out.println(_58_maximumConsecutiveOnesInarray_BFA(arr));
+//		int[] arr = { 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1 }; // 5
+//		System.out.println(_58_maximumConsecutiveOnesInarray_BFA(arr));
+
+		int[] arr = { 5, 7, 7, 8, 8, 10 };
+		int target = 8; // Output: [3, 4]
+
+		System.out.println("Using Linear Iteration: "
+				+ Arrays.toString(_59_first_LastPositionOfNumberInarray_Using_LinearIteration(arr, target)));
+	}
+
+	private static int[] _59_first_LastPositionOfNumberInarray_Using_LinearIteration(int[] arr, int target) {
+		// Time Complexity: O(n)
+		// Space Complexity: O(1)
+		int first = -1;
+		int last = -1;
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == target) {
+				if (first == -1) {
+					first = i;
+				}
+				last = i;
+			}
+		}
+		return new int[] {first,last};
 	}
 
 	private static int _58_maximumConsecutiveOnesInarray_BFA(int[] arr) {
