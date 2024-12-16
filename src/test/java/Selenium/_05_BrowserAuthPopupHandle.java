62b4527eda4c28e46dd8172102bcf5a3583f7b1e
avdhutssh <avdhut.ssh@gmail.com>
2024-12-16 22:21:24 +0530

    BrowserAuthPopupHandle By Using HasAuthenticationInterface

--- src/test/java/Selenium/_05_BrowserAuthPopupHandle.java
+++ src/test/java/Selenium/_05_BrowserAuthPopupHandle.java
@@ -3,6 +3,8 @@ package Selenium;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.HasAuthentication;
+import org.openqa.selenium.UsernameAndPassword;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
@@ -11,22 +13,32 @@ import org.testng.annotations.Test;
 
 public class _05_BrowserAuthPopupHandle {
 	private WebDriver driver;
+
 	@BeforeTest
 	public void setUp() {
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	}
-	
+
 	@AfterTest
 	public void tearDown() {
 		driver.quit();
 	}
-	
+
 	@Test
 	public void _01_ByEnteringCredsInUrl() {
 		driver.get("https://admin:admin@the-internet.herokuapp.com/basic_auth");
-		driver.findElement(By.tagName("p")).getText().trim().equals("Congratulations! You must have the proper credentials.");
+		driver.findElement(By.tagName("p")).getText().trim()
+				.equals("Congratulations! You must have the proper credentials.");
+	}
+
+	@Test
+	public void _02_UsingHasAuthenticationInterface() {
+		((HasAuthentication) driver).register(UsernameAndPassword.of("admin", "admin"));
+		driver.get("https://the-internet.herokuapp.com/basic_auth");
+		driver.findElement(By.tagName("p")).getText().trim()
+				.equals("Congratulations! You must have the proper credentials.");
 	}
 
 }
