1c5ca7c419f089c828ffc86036dd318a684f955d
avdhutssh <avdhut.ssh@gmail.com>
2024-12-16 22:58:28 +0530

    BrowserAuthPopupHandle By Using URI_Predicate

--- src/test/java/Selenium/_05_BrowserAuthPopupHandle.java
+++ src/test/java/Selenium/_05_BrowserAuthPopupHandle.java
@@ -1,6 +1,8 @@
 package Selenium;
 
+import java.net.URI;
 import java.time.Duration;
+import java.util.function.Predicate;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.HasAuthentication;
@@ -36,6 +38,15 @@ public class _05_BrowserAuthPopupHandle {
 	@Test
 	public void _02_UsingHasAuthenticationInterface() {
 		((HasAuthentication) driver).register(UsernameAndPassword.of("admin", "admin"));
+		driver.get("https://the-internet.herokuapp.com/basic_auth");
+		driver.findElement(By.tagName("p")).getText().trim()
+				.equals("Congratulations! You must have the proper credentials.");
+	}
+
+	@Test
+	public void _03_UsingURI_Predicate() {
+		Predicate<URI> uriPredicate = uri -> uri.getHost().contains("the-internet.herokuapp.com");
+		((HasAuthentication) driver).register(uriPredicate, UsernameAndPassword.of("admin", "admin"));
 		driver.get("https://the-internet.herokuapp.com/basic_auth");
 		driver.findElement(By.tagName("p")).getText().trim()
 				.equals("Congratulations! You must have the proper credentials.");
