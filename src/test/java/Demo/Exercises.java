820216f63609c7588785f2d422a6436224364490
avdhutssh <avdhut.ssh@gmail.com>
2025-03-23 22:41:54 +0530

    Exercise 2

--- src/test/java/Demo/Exercises.java
+++ src/test/java/Demo/Exercises.java
@@ -5,6 +5,24 @@ public class Exercises {
 	public static void main(String[] args) {
 		System.out.println(_01_multiply(5, 10)); // 50
 
+		System.out.println("-----------------------------------------");
+		
+		int[] arr1 = { 1, 4, 8, 15, 17 };
+		System.out.println(_02_MaxDiffBetweenAdjacentElementsInArr(arr1)); // 7
+		
+		System.out.println("-----------------------------------------");
+	}
+
+	private static int _02_MaxDiffBetweenAdjacentElementsInArr(int[] arr) {
+		// Time complexity: O(n)
+		// Space complexity: O(1)
+		int maxDiff = Integer.MIN_VALUE;
+		for (int i = 0; i < arr.length-1; i++) {
+			int diff = arr[i+1] - arr[i];
+			maxDiff = Math.max(maxDiff, diff);
+			
+		}
+		return maxDiff;
 	}
 
 	private static int _01_multiply(int num1, int num2) {
