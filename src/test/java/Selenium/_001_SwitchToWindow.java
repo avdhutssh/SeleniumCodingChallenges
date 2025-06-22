733470d43347910e7191827f15b43605107e376b
avdhutssh <avdhut.ssh@gmail.com>
2025-06-22 16:53:37 +0530

    SwitchToWindow using iterator

--- src/test/java/Selenium/_001_SwitchToWindow.java
+++ src/test/java/Selenium/_001_SwitchToWindow.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
@@ -16,76 +18,81 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class _001_SwitchToWindow {
-	WebDriverWait wt;
-	WebDriver driver;
-	JavascriptExecutor jse;
-	int windowNumber = 2;
+    WebDriverWait wt;
+    WebDriver driver;
+    JavascriptExecutor jse;
+    int windowNumber = 2;
 
-	@BeforeTest
-	private void setup() {
-		driver = new ChromeDriver();
-		wt = new WebDriverWait(driver, Duration.ofSeconds(10));
-		jse = (JavascriptExecutor) driver;
-	}
+    @BeforeTest
+    private void setup() {
+        driver = new ChromeDriver();
+        wt = new WebDriverWait(driver, Duration.ofSeconds(10));
+        jse = (JavascriptExecutor) driver;
+    }
 
-	@AfterTest
-	private void tearDown() {
-		driver.quit();
-	}
+    @AfterTest
+    private void tearDown() {
+        driver.quit();
+    }
 
-	@Test
-	private void _01_Using_Iterator() {
+    @Test
+    public void _01_Using_Iterator() {
+        driver.manage().window().maximize();
+        driver.get("https://the-internet.herokuapp.com/windows");
+        String parentWindowID = driver.getWindowHandle();
+        WebElement windowBtn = driver.findElement(By.xpath("//a[@href='/windows/new']"));
+        wt.until(ExpectedConditions.visibilityOf(windowBtn));
+        try {
+            wt.until(ExpectedConditions.elementToBeClickable(windowBtn));
+        } catch (Exception e) {
+            System.out.println("Element is not clickable: " + e.getMessage());
+        }
+        windowBtn.click();
+        Set<String> childWindowsID = driver.getWindowHandles();
+        Iterator<String> it = childWindowsID.iterator();
+        while (it.hasNext()) {
+            String childID = it.next();
+            if (!parentWindowID.equals(childID)) {
+                driver.switchTo().window(childID);
+                String childPageHeading = driver.findElement(By.tagName("h3")).getText();
+                System.out.println("Heading of child window is " + childPageHeading);
+                Assert.assertEquals(childPageHeading, "New Window");
+                driver.close();
+            }
+        }
+        driver.switchTo().window(parentWindowID);
+        String parentPageHeading = driver.findElement(By.tagName("h3")).getText();
+        System.out.println("Heading of child window is " + parentPageHeading);
+        Assert.assertEquals(parentPageHeading, "Opening a new window");
+    }
 
-		driver.manage().window().maximize();
-		driver.get("https://demoqa.com/browser-windows");
-		String parentWindowID = driver.getWindowHandle();
-		WebElement windowBtn = driver.findElement(By.id("windowButton"));
-		jse.executeScript("arguments[0].scrollIntoView(true)", windowBtn);
-		windowBtn.click();
-		Set<String> childWindowsID = driver.getWindowHandles();
-		Iterator<String> it = childWindowsID.iterator();
-		while (it.hasNext()) {
-			String childID = it.next();
-			if (!parentWindowID.equals(childID)) {
-				driver.switchTo().window(childID);
-				String pageHeading = driver.findElement(By.id("sampleHeading")).getText();
-				System.out.println("Heading of child window is " + pageHeading);
-			}
-		}
-		driver.close();
-		driver.switchTo().window(parentWindowID);
-		Assert.assertEquals("DEMOQA", driver.getTitle());
-	}
+    @Test
+    public void _02_Using_Stream_Array() {
+        WebElement windowBtn = driver.findElement(By.id("windowButton"));
+        jse.executeScript("arguments[0].scrollIntoView(true)", windowBtn);
+        windowBtn.click();
 
-	@Test
-	private void _02_Using_Stream_Array() {
-		driver.manage().window().maximize();
-		driver.get("https://demoqa.com/browser-windows");
-		WebElement windowBtn = driver.findElement(By.id("windowButton"));
-		jse.executeScript("arguments[0].scrollIntoView(true)", windowBtn);
-		windowBtn.click();
+        String[] windowHandles = driver.getWindowHandles().stream().toArray(String[]::new);
+        System.out.println("windowHandles.length: " + windowHandles.length);
+        try {
+            if (windowNumber > windowHandles.length) {
+                throw new RuntimeException(String.format(
+                        "The specified window number (%s) is greater than the " + "number of windows created (%s).",
+                        windowNumber, windowHandles.length));
+            }
+            driver.switchTo().window(windowHandles[windowNumber - 1]);
+            System.out.println("Successfully switched to window number: " + windowNumber);
+        } catch (Exception ex) {
+            throw new RuntimeException(
+                    String.format("Failed to switch to window number %s. The total number of open windows was %s.",
+                            windowNumber, windowHandles.length),
+                    ex);
 
-		String[] windowHandles = driver.getWindowHandles().stream().toArray(String[]::new);
-		System.out.println("windowHandles.length: " + windowHandles.length);
-		try {
-			if (windowNumber > windowHandles.length) {
-				throw new RuntimeException(String.format(
-						"The specified window number (%s) is greater than the " + "number of windows created (%s).",
-						windowNumber, windowHandles.length));
-			}
-			driver.switchTo().window(windowHandles[windowNumber - 1]);
-			System.out.println("Successfully switched to window number: " + windowNumber);
-		} catch (Exception ex) {
-			throw new RuntimeException(
-					String.format("Failed to switch to window number %s. The total number of open windows was %s.",
-							windowNumber, windowHandles.length),
-					ex);
-
-		}
-		String pageHeading = driver.findElement(By.id("sampleHeading")).getText();
-		System.out.println("Heading of child window is " + pageHeading);
-		driver.close();
-		driver.switchTo().window(windowHandles[0]);
-		Assert.assertEquals("DEMOQA", driver.getTitle());
-	}
+        }
+        String pageHeading = driver.findElement(By.id("sampleHeading")).getText();
+        System.out.println("Heading of child window is " + pageHeading);
+        driver.close();
+        driver.switchTo().window(windowHandles[0]);
+        Assert.assertEquals("DEMOQA", driver.getTitle());
+    }
 }
