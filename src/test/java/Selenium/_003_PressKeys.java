908ce2701a8ca58b73b43677618d1cd888f1ea10
avdhutssh <avdhut.ssh@gmail.com>
2025-06-22 19:16:18 +0530

    Key Press

--- src/test/java/Selenium/_003_PressKeys.java
+++ src/test/java/Selenium/_003_PressKeys.java
@@ -10,9 +10,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class _003_PressKeys {
     WebDriver driver;
@@ -23,15 +21,57 @@ public class _003_PressKeys {
         driver.get("https://the-internet.herokuapp.com/key_presses");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
     }
 
-    @Test
-    public void _01_PressEnter() {
+    @AfterSuite
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @DataProvider(name = "keysProvider")
+    public Object[][] keysProvider() {
+        return new Object[][]{
+                {Keys.ENTER, "ENTER"},
+                {Keys.TAB, "TAB"},
+                {Keys.SHIFT, "SHIFT"},
+                {Keys.CONTROL, "CONTROL"},
+                {Keys.ALT, "ALT"},
+                {Keys.ESCAPE, "ESCAPE"},
+                {Keys.BACK_SPACE, "BACK_SPACE"},
+                {Keys.SPACE, "SPACE"},
+                {Keys.ARROW_UP, "UP"},
+                {Keys.ARROW_DOWN, "DOWN"},
+                {Keys.ARROW_LEFT, "LEFT"},
+                {Keys.ARROW_RIGHT, "RIGHT"},
+                {Keys.DELETE, "DELETE"},
+                {Keys.HOME, "HOME"},
+                {Keys.END, "END"},
+                {Keys.PAGE_UP, "PAGE_UP"},
+                {Keys.PAGE_DOWN, "PAGE_DOWN"},
+                {Keys.INSERT, "INSERT"},
+                {Keys.F1, "F1"},
+                {Keys.F2, "F2"},
+                {Keys.F3, "F3"},
+                {Keys.F4, "F4"},
+                {Keys.F5, "F5"},
+                {Keys.F6, "F6"},
+                {Keys.F7, "F7"},
+                {Keys.F8, "F8"},
+                {Keys.F9, "F9"},
+                {Keys.F10, "F10"},
+                {Keys.F11, "F11"},
+                {Keys.F12, "F12"}
+        };
+    }
+
+    @Test(dataProvider = "keysProvider")
+    public void _PressKeys(Keys key, String expected) {
         WebElement inputField = driver.findElement(By.id("target"));
         Actions act = new Actions(driver);
-        act.moveToElement(inputField).sendKeys(Keys.ENTER).build().perform();
+        act.moveToElement(inputField).sendKeys(key).perform();
         String resultText = driver.findElement(By.id("result")).getText();
-        Assert.assertEquals(resultText, "You entered: ENTER");
+        Assert.assertEquals(resultText, "You entered: " + expected, "Failed for key: " + expected);
     }
 }
