358d9bf44b9d1b9f8dd318646f8fa1eeed0c5589
avdhutssh <avdhut.ssh@gmail.com>
2025-06-23 09:56:19 +0530

    _06_SelectLowestFlightPriceDate working optimize , handle iframe and scrolling

--- src/test/java/SeleniumAdvanced/_06_SelectLowestFlightPriceDate.java
+++ src/test/java/SeleniumAdvanced/_06_SelectLowestFlightPriceDate.java
@@ -2,6 +2,7 @@ package SeleniumAdvanced;
 
 import org.checkerframework.checker.units.qual.min;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -27,11 +28,30 @@ public class _06_SelectLowestFlightPriceDate {
             closeButtons.get(0).click();
             Thread.sleep(2000);
         }
+        List<WebElement> iframes = driver.findElements(By.id("webklipper-publisher-widget-container-notification-frame"));
+        if (!iframes.isEmpty()) {
+            driver.switchTo().frame(iframes.get(0));
+            List<WebElement> closeBtns = driver.findElements(By.xpath("//button[contains(@id,'_we_wk_close')]"));
+            if (!closeBtns.isEmpty()) {
+                closeBtns.get(0).click();
+                System.out.println("Closed the notification iframe popup.");
+            } else {
+                System.out.println("Close button not found inside iframe.");
+            }
+            driver.switchTo().defaultContent();
+        } else {
+            System.out.println("Notification iframe not present.");
+        }
+
         driver.findElement(By.xpath("//div[@aria-label='Departure Date inputbox']")).click();
+        // String yearMonth = YearMonth.now().toString();  //current month
+        String yearMonth = YearMonth.now().plusMonths(1).toString();
+        String farePath = "//div[@aria-label='month  " + yearMonth + "']//div[not(contains(@class,'outside-month'))]/span/span[contains(text(),'₹')]";
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+        jse.executeScript("window.scrollBy(0,100)");
+        jse.executeScript("arguments[0].scrollIntoView(true);", driver.findElement(By.xpath(farePath)));
         Thread.sleep(2000);
 
-        String yearMonth = YearMonth.now().toString();
-        String farePath = "//div[@aria-label='month  " + yearMonth + "']//div[not(contains(@class,'outside-month'))]/span/span[contains(text(),'₹')]";
         int lowestFare = driver.findElements(By.xpath(farePath))
                 .stream()
                 .mapToInt(ele -> Integer.parseInt(ele.getText().replaceAll("[^0-9]", "")))
@@ -51,8 +71,8 @@ public class _06_SelectLowestFlightPriceDate {
             System.out.println("Date: " + dateText);
         }
 
-        Random rand = new Random();
-        int randomIndex = rand.nextInt(allDatesWithLowestFare.size());
+        Random random = new Random();
+        int randomIndex = random.nextInt(allDatesWithLowestFare.size());
         WebElement selectedDateElem = allDatesWithLowestFare.get(randomIndex);
 
         String selectedDateText = selectedDateElem.getText().split("\\n")[0].trim();
