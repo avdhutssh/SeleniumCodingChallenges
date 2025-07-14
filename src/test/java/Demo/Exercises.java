1cfe888c440bfea7979ee73732e7417982b2b81b
avdhutssh <avdhut.ssh@gmail.com>
2025-07-14 10:40:07 +0530

    Add methods for finding first non-repeating character, grouping anagrams, and finding indices of two numbers that sum to a target in Exercises.java

--- src/test/java/Demo/Exercises.java
+++ src/test/java/Demo/Exercises.java
@@ -1,10 +1,15 @@
 package Demo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
+
+import javax.print.attribute.standard.MediaSize.Other;
 
 public class Exercises {
 
@@ -53,6 +58,106 @@ public class Exercises {
 		System.out.println("-----------------------------------------");
 
 		System.out.println("-----------------------------------------");
+
+		System.out.println(_11_HashMap_firstNonRepeatingChar("leetcode")); // l
+		System.out.println(_11_HashMap_firstNonRepeatingChar("hello")); // h
+		System.out.println(_11_HashMap_firstNonRepeatingChar("aabbcc")); // null
+
+		System.out.println("-----------------------------------------");
+
+		System.out.println(_12_Using_Hashmap_groupAnagrams(new String[] { "eat", "tea", "tan", "ate", "nat", "bat" }));
+		// [[eat, tea, ate], [tan, nat], [bat]]
+		System.out.println(_12_Using_Hashmap_groupAnagrams(new String[] { "abc", "cba", "bac", "foo", "bar" }));
+		// [[abc, cba, bac], [foo], [bar]]
+		System.out.println(_12_Using_Hashmap_groupAnagrams(
+				new String[] { "listen", "silent", "triangle", "integral", "garden", "ranged" }));
+		// [[listen, silent], [triangle, integral], [garden, ranged]]
+
+		System.out.println("-----------------------------------------");
+
+		System.out.println(
+				Arrays.toString(_13_HashMap_indicesOfTwoNumbersThatGivesTarget(new int[] { 2, 7, 11, 15 }, 9)));
+// [0, 1]
+		System.out.println(Arrays.toString(_13_HashMap_indicesOfTwoNumbersThatGivesTarget(new int[] { 3, 2, 4 }, 6)));
+// [1, 2]
+		System.out.println(Arrays.toString(_13_HashMap_indicesOfTwoNumbersThatGivesTarget(new int[] { 3, 3 }, 6)));
+// [0, 1]
+		System.out.println(
+				Arrays.toString(_13_HashMap_indicesOfTwoNumbersThatGivesTarget(new int[] { 1, 2, 3, 4, 5 }, 10)));
+// []
+		System.out.println(
+				Arrays.toString(_13_HashMap_indicesOfTwoNumbersThatGivesTarget(new int[] { 1, 2, 3, 4, 5 }, 7)));
+// [2, 3]
+		System.out.println(
+				Arrays.toString(_13_HashMap_indicesOfTwoNumbersThatGivesTarget(new int[] { 1, 2, 3, 4, 5 }, 3)));
+// [0, 1]
+		System.out.println(Arrays.toString(_13_HashMap_indicesOfTwoNumbersThatGivesTarget(new int[] {}, 0)));
+// []
+
+		System.out.println("-----------------------------------------");
+
+		System.out.println("-----------------------------------------");
+
+		System.out.println("-----------------------------------------");
+
+		System.out.println("-----------------------------------------");
+
+		System.out.println("-----------------------------------------");
+
+		System.out.println("-----------------------------------------");
+
+		System.out.println("-----------------------------------------");
+
+		System.out.println("-----------------------------------------");
+
+		System.out.println("-----------------------------------------");
+
+		System.out.println("-----------------------------------------");
+	}
+
+	private static int[] _13_HashMap_indicesOfTwoNumbersThatGivesTarget(int[] arr, int target) {
+
+		HashMap<Integer, Integer> hm = new HashMap<>();
+		for (int i = 0; i < arr.length; i++) {
+			int otherNum = target - arr[i];
+			if (hm.containsKey(otherNum)) {
+				return new int[] { hm.get(otherNum), i };
+			}
+			hm.put(arr[i], i);
+		}
+		return new int[] {};
+	}
+
+	private static ArrayList<List<String>> _12_Using_Hashmap_groupAnagrams(String[] strArr) {
+
+		HashMap<String, List<String>> anagramGroup = new HashMap<>();
+		for (String str : strArr) {
+			char[] chars = str.toCharArray();
+			Arrays.sort(chars);
+			String sortedStr = new String(chars);
+			if (anagramGroup.containsKey(sortedStr)) {
+				anagramGroup.get(sortedStr).add(str);
+			} else {
+				List<String> group = new ArrayList<>();
+				group.add(str);
+				anagramGroup.put(sortedStr, group);
+			}
+		}
+		return new ArrayList<List<String>>(anagramGroup.values());
+	}
+
+	private static Character _11_HashMap_firstNonRepeatingChar(String str) {
+
+		HashMap<Character, Integer> hm = new HashMap<>();
+		for (char ch : str.toCharArray()) {
+			hm.put(ch, hm.getOrDefault(ch, 0) + 1);
+		}
+		for (char ch : str.toCharArray()) {
+			if (hm.get(ch) == 1) {
+				return ch;
+			}
+		}
+		return null;
 	}
 
 	private static void _09_HashSeT_findDuplicatesInArray(int[] nums) {
